628e6e7a1af04807eefccc3784230402ef07c253
Praveen Patidar <102358171+praveen88p@users.noreply.github.com>
2024-10-14 00:40:32 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seating_chart_project.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seating_chart_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +12,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Check if the PORT environment variable is set, and use it if available
+    port = os.environ.get('PORT', '8000')  # Default to port 8000 if PORT isn't set
+    sys.argv = sys.argv[:1] + ['runserver', '0.0.0.0:' + port] + sys.argv[1:]
+
     execute_from_command_line(sys.argv)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
