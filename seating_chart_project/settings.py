20e34a3d3808a832f285911cf85dc08d7f50971e
Praveen Patidar <102358171+praveen88p@users.noreply.github.com>
2024-10-13 23:27:20 +0530

    Update settings.py

--- seating_chart_project/settings.py
+++ seating_chart_project/settings.py
@@ -79,11 +79,7 @@ WSGI_APPLICATION = "seating_chart_project.wsgi.application"
 
 # Use SQLite in development and switch to PostgreSQL in production
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Password validation
